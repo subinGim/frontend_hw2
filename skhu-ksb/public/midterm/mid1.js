0306902ecd5507b43747f5245525039cb7400685
Gim, subin <vk0218@naver.com>
2020-05-06 09:40:54 +0900

    second commit_mid

--- skhu-ksb/public/midterm/mid1.js
+++ skhu-ksb/public/midterm/mid1.js
@@ -2,5 +2,11 @@ let a = [];
 for(let i=0; i<100; i++){
     a[i] = Math.floor(Math.random()*100+1);
 }
-
-//let d = a.filter((num) => num % 2 === 0);
+console.log(a);
+for(let i=0; i<a.length; i++){
+    let b = a[i]/10; //십의 자리
+    let c = a[i] - b*10; //일의 자리
+    if(c >= 5 && c <= 9)
+        a.splice(i,1);
+}
+console.log(a);
